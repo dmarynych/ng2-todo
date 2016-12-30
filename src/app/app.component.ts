d189645bca66bb33411a9cbba183379927f77268
Zhenya <EvgeniyWide@gmail.com>
2016-12-30 17:35:52 +0200

    move TodoDataService to root scope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { TodoDataService } from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
